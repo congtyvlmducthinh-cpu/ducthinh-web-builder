f4c54edbc4535d67606fe894df95d0c95e243fde
vinhduc <vinhduc@ducthinh.vn>
2026-07-01 08:53:09 +0700

    chore: cleanup temp/debug files

--- sites/kiem-tra-gia/_debug_search9.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('vi.html', 'utf-8');
-const m = html.match(/<script>([\s\S]*?)<\/script>/);
-const js = m[1];
-
-// Check populateFilters function
-const pfIdx = js.indexOf('function populateFilters');
-console.log('=== populateFilters ===');
-console.log(js.substring(pfIdx, pfIdx + 3000));
