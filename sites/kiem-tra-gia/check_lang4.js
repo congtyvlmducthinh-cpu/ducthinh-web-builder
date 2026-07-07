26b940d6da45665286c28f30683f0e3f2094a645
vinhduc <vinhduc@ducthinh.vn>
2026-07-07 08:08:11 +0700

    chore: cleanup temp scripts

--- sites/kiem-tra-gia/check_lang4.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('vi.html', 'utf8');
-const idx = c.lastIndexOf('lang-switcher');
-const start = c.lastIndexOf('<div', idx);
-const end = c.indexOf('</div>', idx + 50) + 6;
-console.log('vi.html lang-switcher block:');
-console.log(JSON.stringify(c.substring(start, end)));
