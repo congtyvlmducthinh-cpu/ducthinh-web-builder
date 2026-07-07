c6e66b240b90dc5dbae66b0956fde4dd1f711b13
vinhduc <vinhduc@ducthinh.vn>
2026-07-07 08:01:43 +0700

    chore: cleanup temp scripts

--- sites/kiem-tra-gia/check_zh.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let zh = fs.readFileSync('zh.html', 'utf8');
-let checks = ['Vui lòng nhập','Email người phụ trách','Chưa có sản phẩm','Đã copy','cập nhật','Ngày báo giá','BÁO GIÁ','Xem báo giá','In báo giá'];
-for(let c of checks){
-  let n = (zh.match(new RegExp(c.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g')) || []).length;
-  if(n>0) console.log('STILL HAS:', c, 'x' + n);
-  else console.log('OK:', c);
-}
