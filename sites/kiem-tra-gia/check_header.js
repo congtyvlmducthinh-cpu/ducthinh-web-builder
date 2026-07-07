506b099cca876811675073a5aa74e566c8f2f47a
vinhduc <vinhduc@ducthinh.vn>
2026-07-07 07:55:14 +0700

    chore: cleanup temp scripts

--- sites/kiem-tra-gia/check_header.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('vi.html', 'utf8');
-
-// Show area around header to verify
-let headerStart = c.indexOf('<header');
-let headerEnd = c.indexOf('</header>');
-console.log('=== HEADER CONTENT ===');
-console.log(c.substring(headerStart, Math.min(headerEnd+20, headerStart+500)));
-console.log('=== END HEADER ===');
--- sites/kiem-tra-gia/find_badges.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('vi.html', 'utf8');
-
-// Find all badge spans
-let idx = c.indexOf('class="badge"');
-while(idx >= 0) {
-  console.log(c.substring(Math.max(0,idx-40), idx+100));
-  idx = c.indexOf('class="badge"', idx+1);
-}
